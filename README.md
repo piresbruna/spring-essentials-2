17adeaf3e4225ddccf07b837ad409e51ab53be24
Bruna S. Pires <69404377+piresbruna@users.noreply.github.com>
2022-05-17 21:34:17 -0300

    Método GET
    Mapeamento para mais de uma url. Criação da classe de serviço com os métodos de listagem e busca por id. Adição de notação lombok na classe Anime para geração automática de getters e setters. Utilização dos métodos da classe de serviço pela classe Controller. Modificação do @RequestMapping para o plural, de acordo com a arquitetura REST.

--- README.md
+++ README.md
@@ -3,9 +3,20 @@ Prática do framework Spring na construção de uma API REST.
 
 **Reprodução do [Curso Spring Boot 2 Essentials](https://www.youtube.com/playlist?list=PL62G310vn6nFBIxp6ZwGnm8xMcGE3VA5H)**
 
+## Estrutura
+
+### springboot2
+
+Contém o projeto construído nos vídeos 1 à 5.
+
+### springboot2-spring-io
+
+Contém o projeto iniciado no vídeo 6, contruído usando o [Spring Initializr](https://start.spring.io).
+
 ## Commits seguindo os vídeos
 
 - **Commit 2:** Vídeos 1, 2, 3 e 4;
 	- A variável do tipo DateUtil  na classe AnimeController é final. Correção no commit seguinte.
 - **Commit 3:** Vídeo 5;
 - **Commit 4:** Vídeo 6;
+- **Commit 5:** Vídeos 7 e 8;
--- springboot2-spring-io/springboot2/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ springboot2-spring-io/springboot2/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,10 +1,13 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +15,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
     private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
-    @GetMapping(path = "list")
-    public List<Anime> list(){
+    @GetMapping
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Boku no Hero Academia"), new Anime("Berserk"), new Anime("One Piece"));
+        return ResponseEntity.ok(animeService.listAll());
     }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.findById(id));
+    }
 }
